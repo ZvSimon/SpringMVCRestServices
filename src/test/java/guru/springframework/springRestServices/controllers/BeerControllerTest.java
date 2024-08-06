d067358ce638687cf02634988f3f2dd4987adfc4
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-08-06 19:13:38 +0200

    MockMVC Test List Beers

--- src/test/java/guru/springframework/springRestServices/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/springRestServices/controllers/BeerControllerTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
 import static org.hamcrest.core.Is.is;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -25,6 +27,22 @@ class BeerControllerTest {
     BeerService beerService;
 
     BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
+
+    @Test
+    void testListBeers() throws Exception {
+        given(beerService.listBeers()).willReturn(beerServiceImpl.listBeers());
+
+        mockMvc.perform(get("/api/v1/beer")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].id", is(beerServiceImpl.listBeers().get(0).getId().toString())))
+                .andExpect(jsonPath("$[0].beerName", is(beerServiceImpl.listBeers().get(0).getBeerName())))
+                .andExpect(jsonPath("$[1].id", is(beerServiceImpl.listBeers().get(1).getId().toString())))
+                .andExpect(jsonPath("$[1].beerName", is(beerServiceImpl.listBeers().get(1).getBeerName())));
+    }
+
+
 
     @Test
     void getBeerById() throws Exception {
